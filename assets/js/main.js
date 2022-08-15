e79340bc23bc9075c31191724fe94996a6836f65
britofelipe <arrudabritofelipe@gmail.com>
2022-08-15 14:25:51 -0300

    updating home, popular and value session

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,64 @@
+// /*=============== SHOW MENU ===============*/
+// const navMenu = documnet.getElementById('nav-menu'),
+// navToggle = documnet.getElementById('nav-toggle'),
+// navClose = documnet.getElementById('nav-close')
+
+// /*=============== MENU SHOW ===============*/
+// /*=============== Validate if constant exists ===============*/
+// if(navToggle){
+//     navToggle.addEventListener('click', () =>{
+//         navMenu.classList.add('show-menu')
+//     })
+// }
+
+// /*=============== MENU HIDDEN ===============*/
+// /*=============== Validate if constant exists ===============*/
+// if(navClose){
+//     navClose.addEventListener('click', () =>{
+//         navMenu.classList.add('show-menu')
+//     })
+// }
+// /*=============== REMOVE MENU MOBILE ===============*/
+// const navLink = document.querySelectorAll('.nav__link')
+
+// function linkAction(){
+//     const navMenu = document.getElementById('nav-menu')
+//     // When we click on eack nav__link, we remove the show-menu class
+//     navMenu.classList.remove('show-menu')
+// }
+
+// navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater tha 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: true,
+})
+
 
 
 /*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 /*=============== VALUE ACCORDION ===============*/
 
